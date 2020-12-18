815ae720d6af2a22afe15c660450b8e0f4b19e9f
Roman Tataurov <roman.tataurov@yandex.ru>
2020-12-18 16:45:24 +0300

    Removed redundant modules inclusion

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// https://github.com/hexojs/hexo-fs
-var fs = require('hexo-fs');
-
 hexo.extend.filter.register('after_init', require('./lib/after_init'));
 hexo.extend.helper.register('manifest_asset_path', require('./lib/manifest_asset_path'));
+
